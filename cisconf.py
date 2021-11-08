49f0e14e4d4985447ab46ecedad93ee54fc5951e
Louis de Granrut <46935569+LouisDeGranrut@users.noreply.github.com>
2021-11-08 16:27:43 +0100

    Update cisconf.py

--- cisconf.py
+++ cisconf.py
@@ -4,9 +4,9 @@ print("The Cisco configuration file creator")
 print("by Louis de Granrut")
 print("=============================")
 
-print("Create config file for:")
-print("1 - Switch")
-print("2 - Router")
+print("Create config file for a:")
+print("- Switch")
+print("- Router")
 
 answer = input("")
 if(answer == "Switch" or answer == "switch" or answer == "s"):
@@ -14,12 +14,10 @@ if(answer == "Switch" or answer == "switch" or answer == "s"):
 else :
     print("Router")
 
-#-------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------
 hostname = input("Enter machine name: ")
 password = input("Enter Enable password: ")
 
-
-#configure interface (switchport, ip add, no shut)
 #ospf
 #ip routing
 #dhcp
@@ -29,25 +27,26 @@ f.write("en\n")
 f.write("conf t\n")
 f.write("hostname " + hostname + "\n")
 f.write("enable secret " + password + "\n")
+f.write("\n")
 
 command = input("")
 
 while(command != "end"):
     
-    #vlan-----------------------------------------------------------------------------------------------
+    #vlan-----------------------------------------------------------------------------------------------------
     if (command == "vlan" or command == "v"):
         numb = input("Vlan number: ")
         ipadd = input("Vlan address: ")
-        f.write("vlan " + numb + "\n interface vlan " + numb + "\n ip add " + ipadd + "\n ex \n")
+        f.write("vlan " + numb + "\n interface vlan " + numb + "\n ip add " + ipadd + "\n ex \n \n")
         command = input("")
 
-    #interface------------------------------------------------------------------------------------------
+    #interface------------------------------------------------------------------------------------------------
     if (command == "interface" or command == "int" or command == "i"):
         intname = input("Interface Name: ")
         ipadd = input("Interface address: ")
         #encapsulation dot1Q
-        switchport = input("Switchport mode: ")
-        f.write("interface " + intname + "\n ip add " + ipadd + "\n" + switchport+ "\n no shut\n ex \n")
+        switchport = input("Switchport: ")
+        f.write("interface " + intname + "\n ip add " + ipadd + "\n" + switchport+ "\n no shut\n ex \n \n")
         command = input("")
     #interface range------------------------------------------------------------------------------------------
     if (command == "interface range" or command == "ir"):
