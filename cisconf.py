14d3d86a6adc094095a6fb184b13fcdab3d897fd
Louis de Granrut <46935569+LouisDeGranrut@users.noreply.github.com>
2021-11-08 11:37:03 +0100

    Create cisconf.py

---
+++ cisconf.py
@@ -0,0 +1,54 @@
+print("=====CISCO CONFIG EDITOR=====")
+print("Welcome to Cisconf")
+print("The Cisco configuration file creator")
+print("by Louis de Granrut")
+print("=============================")
+
+print("Create config file for:")
+print("1 - Switch")
+print("2 - Router")
+
+answer = input("")
+if(answer == "1"):
+    print("Switch")
+else :
+    print("Router")
+
+hostname = input("Enter machine name: ")
+password = input("Enter Enable password: ")
+
+
+#configure every interface (switchport, ip add, no shut)
+#ospf
+#routes
+#vlan X ex interface vlan X ip add ex
+
+f = open("configuration.txt", "a")
+f.write("en\n")
+f.write("conf t\n")
+f.write("hostname " + hostname + "\n")
+f.write("enable secret " + password + "\n")
+
+command = input("")
+
+while(command != "end"):
+    #vlan
+    if (command == "vlan"):
+        numb = input("Vlan number: ")
+        ipadd = input("Vlan address: ")
+        f.write("vlan " + numb + "\n interface vlan " + numb + "\n ip add " + ipadd + "\n ex \n")
+        command = input("")
+
+    #interface
+    if (command == "interface"):
+        intname = input("Interface Name: ")
+        ipadd = input("Interface address: ")
+        f.write("interface " + intname + "\n ip add " + ipadd + "\n no shut\n ex \n")
+        command = input("")
+    #....
+
+f.write("wr\n")
+f.close()
+print("Config file created !")
+
+
