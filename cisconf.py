1f295fbfd2475cc7f5ed712e7793a4ad9648cf5b
Louis de Granrut <46935569+LouisDeGranrut@users.noreply.github.com>
2021-11-08 14:08:24 +0100

    Update cisconf.py

--- cisconf.py
+++ cisconf.py
@@ -8,21 +8,21 @@ print("Create config file for:")
 print("1 - Switch")
 print("2 - Router")
 
-#-------------------------------------------------------------------------------------------------------
 answer = input("")
-if(answer == "1"):
+if(answer == "Switch" or answer == "switch" or answer == "s"):
     print("Switch")
 else :
     print("Router")
 
+#-------------------------------------------------------------------------------------------------------
 hostname = input("Enter machine name: ")
 password = input("Enter Enable password: ")
 
 
-#configure every interface (switchport, ip add, no shut)
+#configure interface (switchport, ip add, no shut)
 #ospf
-#spanning tree
 #ip routing
+#dhcp
 
 f = open("configuration.txt", "a")
 f.write("en\n")
@@ -35,19 +35,28 @@ command = input("")
 while(command != "end"):
     
     #vlan-----------------------------------------------------------------------------------------------
-    if (command == "vlan"):
+    if (command == "vlan" or command == "v"):
         numb = input("Vlan number: ")
         ipadd = input("Vlan address: ")
         f.write("vlan " + numb + "\n interface vlan " + numb + "\n ip add " + ipadd + "\n ex \n")
         command = input("")
 
     #interface------------------------------------------------------------------------------------------
-    if (command == "interface"):
+    if (command == "interface" or command == "int" or command == "i"):
         intname = input("Interface Name: ")
         ipadd = input("Interface address: ")
         #encapsulation dot1Q
         switchport = input("Switchport mode: ")
-        f.write("interface " + intname + "\n ip add " + ipadd + "\n" + "switchport\n no shut\n ex \n")
+        f.write("interface " + intname + "\n ip add " + ipadd + "\n" + switchport+ "\n no shut\n ex \n")
+        command = input("")
+    #interface range------------------------------------------------------------------------------------------
+    if (command == "interface range" or command == "ir"):
+        intname = input("Interface Name: ")
+        to = input("To: ")
+        #spanning tree
+        channelgroup = input("Channel Group number: ")
+        channelgroupMode = input("Channel Group Mode (auto, desirable, on, active, passive): ")
+        f.write("interface range " + intname + "-" + to + "\n channel-group " + channelgroup + " mode " + channelgroupMode + "\n";
         command = input("")
     #....
 
